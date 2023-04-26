936e9177fa3bfff77bb3753a9c944f04764b8b1a
Kirsten <amayak@email.unc.edu>
2023-04-25 21:30:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -83,6 +83,62 @@ app.get("/homepage", (req, res) => {
   res.send(`
     <h1>Welcome to MyList!</h1>
     <h2>Hello, ${req.query.username}!</h2>
+    <h2>Add a Task</h2>
+<form id="add-task-form">
+  <label for="task-title">Task Title:</label>
+  <input type="text" id="task-title" name="task-title">
+  <label for="task-description">Task Description:</label>
+  <input type="text" id="task-description" name="task-description">
+  <label for="task-date">Task Date:</label>
+  <input type="date" id="task-date" name="task-date">
+  <button type="submit">Add Task</button>
+</form>
+<ul id="task-list"></ul>
+<script>
+  function addTask() {
+    // Get the input field and the task list
+    var input = document.getElementById("taskInput");
+    var list = document.getElementById("taskList");
+
+    // Create a new list item and add it to the list
+    var item = document.createElement("li");
+    item.innerHTML = input.value;
+    list.appendChild(item);
+
+    // Clear the input field
+    input.value = "";
+  }
+
+
+  const addTaskForm = document.querySelector('#add-task-form');
+  const taskTitleInput = document.querySelector('#task-title');
+  const taskDescriptionInput = document.querySelector('#task-description');
+  const taskDateInput = document.querySelector('#task-date');
+  const taskList = document.querySelector('#task-list');
+  
+  
+
+  addTaskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const task = {
+      title: taskTitleInput.value,
+      description: taskDescriptionInput.value,
+      date: taskDateInput.value
+    };
+
+      // Clear the form inputs
+    taskTitleInput.value = '';
+      taskDescriptionInput.value = '';
+      taskDateInput.value = '';
+  })
+   //.catch(error => console.log(error));
+ // });
+   // }
+</script>
+<ul id="planner-list">
+  <!-- Existing tasks will be added here -->
+</ul>
   `);
 });
 
