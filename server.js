db9061fb787f9bc0cab3341963bd3e72e5086e61
Levi Harris <69766046+leharris3@users.noreply.github.com>
2023-05-09 16:53:41 -0400

    some styling

--- server.js
+++ server.js
@@ -7,14 +7,59 @@ app.use(bodyParser.json());
 // Initialize an empty list of users
 const users = [];
 
-// Homepage
 app.get("/", (req, res) => {
   // Render the homepage with two buttons and a subtitle
   res.send(`
-    <h1>Welcome to MyList!</h1>
-    <h2>Please Signup to Continue</h2>
-    <button onclick="window.location.href='/login'">Login</button>
-    <button onclick="window.location.href='/signup'">Signup</button>
+    <html>
+      <head>
+        <style>
+          body {
+            background-color: cream;
+            font-family: Futura, sans-serif;
+            color: black;
+          }
+          .container {
+            margin: 0 auto;
+            max-width: 600px;
+            padding: 100px;
+            border: 5px solid lightgray;
+            border-radius: 10px;
+            text-align: center;
+          }
+          h1 {
+            font-size: 3em;
+            margin-bottom: 5px;
+          }
+          h2 {
+            font-size: 2em;
+            font-weight: normal;
+            margin-bottom: 10px;
+          }
+          button {
+            font-size: 1.5em;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            color: white;
+            background-color: lightblue;
+            cursor: pointer;
+            margin-right: 10px;
+            margin-bottom: 10px;
+          }
+          button:hover {
+            background-color: #3e8e41;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1>Welcome to Planify!</h1>
+          <h2>Please Signup to Continue</h2>
+          <button onclick="window.location.href='/login'">Login</button>
+          <button onclick="window.location.href='/signup'">Signup</button>
+        </div>
+      </body>
+    </html>
   `);
 });
 
@@ -81,9 +126,135 @@ app.post("/signup", (req, res) => {
 app.get("/homepage", (req, res) => {
   // Render the homepage with a welcome message for the authenticated user
   res.send(`
-    <h1>Welcome to MyList!</h1>
-    <h2>Hello, ${req.query.username}!</h2>
-  `);
+  <h1>Welcome to Planify, ${req.query.username}!</h1>
+    <h2>Add a Task</h2>
+
+
+// <form id="add-task-form">
+//   <label for="task-title">Task Title:</label>
+//   <input type="text" id="task-title" name="task-title">
+//   <label for="task-description">Task Description:</label>
+//   <input type="text" id="task-description" name="task-description">
+//   <label for="task-date">Task Date:</label>
+//   <input type="date" id="task-date" name="task-date">
+//   <span onclick="newElement()" class="addBtn">Add</span>
+// </form>
+
+// <ul id="task-list"></ul>
+
+// <script>
+
+//   const addTaskForm = document.querySelector('#add-task-form');
+//   const taskTitleInput = document.querySelector('#task-title');
+//   const taskDescriptionInput = document.querySelector('#task-description');
+//   const taskDateInput = document.querySelector('#task-date');
+//   const taskList = document.querySelector('#task-list');
+  
+
+//   addTaskForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
+  
+//     const task = {
+//       title: taskTitleInput.value,
+//       description: taskDescriptionInput.value,
+//       date: taskDateInput.value
+//     };
+
+       // Clear the form inputs
+//       taskTitleInput.value = '';
+//       taskDescriptionInput.value = '';
+//       taskDateInput.value = '';
+//   });
+// </script>
+
+
+// new stuff
+
+<html>  
+<head>
+<style>
+.submitBtn {
+  padding: 10px;
+  background: #90ee90;
+  width: 100%;
+  color: #555;
+  float: left;
+  text-align: center;
+  font-size: 18px;
+  transition: 0.3s;
+  border-radius: 0;
+}
+  .taskfield {
+    padding: 5px;
+    width: 150%;
+    color: #555;
+    float: left;
+    text-align: left;
+    font-size: 16px;
+    //cursor: pointer;
+    border-radius: 0;
+    }
+  .descriptionfield {
+    padding: 5px;
+    width: 150%;
+    color: #555;
+    float: left;
+    text-align: left;
+    font-size: 16px;
+    border-radius: 0;
+    }
+  .datefield {
+    padding: 5px;
+    width: 150%;
+    color: #555;
+    float: left;
+    text-align: left;
+    font-size: 16px;
+    border-radius: 0;
+    }
+</style>  
+
+<title> ToDo Page </title>  
+</head>  
+
+<body align="center">  
+<h2> Create a New Task </h2>  
+<table cellspacing="2" align="center" cellpadding="8" border="0">  
+
+<tr><td> Task </td>   
+<td><input type="text" placeholder="Enter a title for this task" class="taskfield" id="taskTitle"></td></tr>  
+
+<tr><td> Description </td>  
+<td><input type="text" placeholder="Elaborate more on this task..." class="descriptionfield" id="description"></td></tr>
+
+<tr><td> Date </td>  
+<td><input type="date" name="task-date" class="datefield" id="dateInput"></td></tr>
+
+<td><input type="submit" value="Add Task" name="Add Task" onclick="newTask()" class="submitBtn"</td>
+
+</table>
+</script>
+
+<h1>My To Do List</h1>
+<ul id="taskList"></ul>
+
+<script>
+function newTask() {
+let data = ["listitem1", "listitem2"];
+let list = document.getElementById("taskList");
+ 
+data.forEach((item) => {
+  let li = document.createElement("li");
+  li.innerText = item;
+  list.appendChild(li);
+});
+}
+
+</script>
+</body>
+</html>
+
+`);
 });
 
 // Start server
